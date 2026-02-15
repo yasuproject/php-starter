877094912eb193ed9953dd98803ed32e2ce19ec6
yasunurumom <yasunurumom@gmail.com>
2026-02-15 21:35:09 +0000

    Add HomeController and vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'php-starter/app',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5e91ae537f1c3ed6af0676020a7a98d3a628bbfc',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'php-starter/app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5e91ae537f1c3ed6af0676020a7a98d3a628bbfc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
