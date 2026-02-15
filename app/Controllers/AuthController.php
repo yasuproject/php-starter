5e91ae537f1c3ed6af0676020a7a98d3a628bbfc
yasunurumom <yasunurumom@gmail.com>
2026-02-15 21:33:26 +0000

    Fix MVC structure - move view to Views folder

---
+++ app/Controllers/AuthController.php
@@ -0,0 +1,11 @@
+<?php
+
+class AuthController {
+    public function login() {
+        require __DIR__ . '/../Views/login.php';
+    }
+
+    public function authenticate() {
+        // Handle login logic
+    }
+}
--- public/index.php
+++ public/index.php
@@ -2,6 +2,23 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = require __DIR__ . '/../app/Config/app.php';
+$requestUri = $_SERVER['REQUEST_URI'];
+$path = parse_url($requestUri, PHP_URL_PATH);
 
-echo "Welcome to MVC!";
+$routes = [
+    '/' => ['controller' => 'HomeController', 'method' => 'index'],
+    '/login' => ['controller' => 'AuthController', 'method' => 'login'],
+    '/admin/login' => ['controller' => 'AuthController', 'method' => 'login'],
+];
+
+if (isset($routes[$path])) {
+    $controllerName = $routes[$path]['controller'];
+    $methodName = $routes[$path]['method'];
+    
+    require_once __DIR__ . '/../app/Controllers/' . $controllerName . '.php';
+    $controller = new $controllerName();
+    $controller->$methodName();
+} else {
+    http_response_code(404);
+    echo "404 - Page not found";
+}
